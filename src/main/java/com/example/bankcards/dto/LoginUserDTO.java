b0f19ef3cc47452bd83525737c088526ec8594b9
Sergey-Spodarev <spodarev8@gmail.com>
2025-10-08 19:54:54 +0300

    Implemented a LoginUserDTO

---
+++ src/main/java/com/example/bankcards/dto/LoginUserDTO.java
@@ -0,0 +1,42 @@
+package com.example.bankcards.dto;
+
+import com.example.bankcards.entity.Role;
+
+public class LoginUserDTO {
+    private Long id;
+    private String username;
+    private String password;
+    private Role role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
