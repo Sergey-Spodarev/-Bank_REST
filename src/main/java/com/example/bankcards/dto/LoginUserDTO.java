3173f6ca4a4b8c8ba9668bc87a66a4e245f16e7b
Sergey-Spodarev <spodarev8@gmail.com>
2025-10-10 17:31:26 +0300

    Implemented LoginUserDTO

--- src/main/java/com/example/bankcards/dto/LoginUserDTO.java
+++ src/main/java/com/example/bankcards/dto/LoginUserDTO.java
@@ -1,42 +1,23 @@
 package com.example.bankcards.dto;
 
 import com.example.bankcards.entity.Role;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class LoginUserDTO {
-    private Long id;
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String username;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
+
+    @NotNull(message = "Role is required")
     private Role role;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
 }
