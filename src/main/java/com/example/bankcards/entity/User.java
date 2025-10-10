9d3f105486cc8368d1e8ac5edaa1155a525bf14c
Sergey-Spodarev <spodarev8@gmail.com>
2025-10-10 17:31:12 +0300

    Implemented User

--- src/main/java/com/example/bankcards/entity/User.java
+++ src/main/java/com/example/bankcards/entity/User.java
@@ -1,7 +1,11 @@
 package com.example.bankcards.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "users")
 public class User {
@@ -19,35 +23,4 @@ public class User {
     @Column(nullable = false)
     private Role role;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
 }
